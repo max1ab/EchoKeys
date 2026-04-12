aaffddd05127bb0a3dac3de22393b85d10d6cd5e
max1ab <m338383154@gmail.com>
2026-04-12 12:14:26 +0800

    Initial Commit

---
+++ piano-learn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  piano-learn
+//
+//  Created by r00t on 2026/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ piano-learn/piano_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  piano_learnApp.swift
+//  piano-learn
+//
+//  Created by r00t on 2026/4/12.
+//
+
+import SwiftUI
+
+@main
+struct piano_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
